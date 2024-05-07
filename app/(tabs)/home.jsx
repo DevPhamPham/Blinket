44f8111b141f67882dfb56e0bc15bd991c6e7f08
DevPhamPham <pham.duykhoa1303@gmail.com>
2024-05-07 15:56:31 +0700

    update setup, file and folder structure, bottom navbar

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HOme = () => {
+  return (
+    <View>
+      <Text>HOme</Text>
+    </View>
+  )
+}
+
+export default HOme
+
+const styles = StyleSheet.create({})
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from "../constants"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Blinket!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.blinket_logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl font-bold text-center text-white">Khám phá khả năng vô tận với {' '}
+              <Text className="text-secondary-200">Blinket</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-1 -right-0"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-gray-100 mt-7 text-center text-sm font-pregular">
+            Nơi chia sẻ những khoảng khắc: bắt
+            tay vào hành trình khám phá Blinket
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
